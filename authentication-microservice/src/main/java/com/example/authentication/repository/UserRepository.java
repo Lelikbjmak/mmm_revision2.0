6e5a2e9bf2f566cbb48664f4f19d50cc857e7cfb
Lelikbjmak <denissolovey990@gmail.com>
2023-02-23 20:31:24 +0300

    User repository

---
+++ authentication-microservice/src/main/java/com/example/authentication/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.example.authentication.repository;
+
+import com.example.authentication.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByMail(String mail);
+
+}
