c6a772d056faa6f7e7b94974cf402a5935cee3f3
Lelikbjmak <denissolovey990@gmail.com>
2023-02-23 23:40:32 +0300

    validation token mapping

--- authentication-microservice/src/main/java/com/example/authentication/controller/AuthenticationController.java
+++ authentication-microservice/src/main/java/com/example/authentication/controller/AuthenticationController.java
@@ -1,9 +1,15 @@
 package com.example.authentication.controller;
 
 import com.example.authentication.dto.AuthenticationRequest;
+import com.example.authentication.dto.AuthenticationValidationResponse;
 import com.example.authentication.service.AuthenticationService;
+import com.example.authentication.service.JwtService;
+import com.example.authentication.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.ws.rs.HttpMethod;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +20,10 @@ import org.springframework.web.bind.annotation.*;
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
+    private final JwtService jwtService;
+
+    private final UserService userService;
+
     @PostMapping("signIn")
     @ResponseStatus(HttpStatus.ACCEPTED)
     public String authenticateUser(@RequestBody AuthenticationRequest authenticationRequest) {
@@ -21,4 +31,19 @@ public class AuthenticationController {
         return authenticationService.authenticateUser(authenticationRequest);
     }
 
+    @GetMapping("validateToken")
+    @ResponseStatus(HttpStatus.OK)
+    public AuthenticationValidationResponse validateToken(HttpServletRequest request){
+
+        String token = request.getHeader(HttpHeaders.AUTHORIZATION).substring(7);
+        String username = jwtService.extractUsername(token);
+        userService.findByUsername(username).getAuthorities().forEach(System.out::println);
+        System.err.println(token);
+        return AuthenticationValidationResponse.builder()
+                .isAuthenticated(true)
+                .username(username)
+                .methodType(HttpMethod.GET)
+                .authorities(userService.findByUsername(username).getAuthorities().toString())
+                .build();
+    }
 }
