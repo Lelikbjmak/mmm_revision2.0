3c8e7516028b4c2e5bd4c7683cc90a097d398bf4
Lelikbjmak <denissolovey990@gmail.com>
2023-02-23 23:42:21 +0300

    Role entity

---
+++ user-service/src/main/java/com/example/user/model/Role.java
@@ -0,0 +1,27 @@
+package com.example.user.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+@Entity
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    @Enumerated(EnumType.STRING)
+    private EnumRole role;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> userSet;
+}
