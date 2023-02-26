23ed4888c1c443283de8bd983ee9a804cbb2081f
Lelikbjmak <denissolovey990@gmail.com>
2023-02-26 15:11:09 +0300

    refactor response after unsuccessful token validation for any secured endpoint

--- api-gateway/src/main/java/com/example/gateway/security/AuthenticationPrefilter.java
+++ api-gateway/src/main/java/com/example/gateway/security/AuthenticationPrefilter.java
@@ -1,8 +1,10 @@
 package com.example.gateway.security;
 
-import com.example.gateway.dto.AuthenticationValidationResponse;
+import com.example.gateway.dto.AuthenticationValidationFailedResponse;
+import com.example.gateway.dto.AuthenticationValidationSuccessResponse;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.ws.rs.HttpMethod;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +14,7 @@ import org.springframework.core.io.buffer.DataBufferFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
@@ -20,6 +23,7 @@ import org.springframework.web.reactive.function.client.WebClientResponseExcepti
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Objects;
 import java.util.function.Predicate;
@@ -48,30 +52,59 @@ public class AuthenticationPrefilter extends AbstractGatewayFilterFactory<Authen
             String bearerToken = Objects.requireNonNull(request.getHeaders().getFirst(HttpHeaders.AUTHORIZATION)).substring(7);
             log.info("Bearer Token: " + bearerToken);
 
-            if(isSecured.test(request)) {
+            if (isSecured.test(request)) {
+
                 return webClientBuilder.build().get()
                         .uri("http://authentication-microservice/api/auth/validateToken")
                         .header(HttpHeaders.AUTHORIZATION, "Bearer " + bearerToken)
-                        .retrieve().bodyToMono(AuthenticationValidationResponse.class)
+                        .retrieve().bodyToMono(AuthenticationValidationSuccessResponse.class)
                         .map(response -> {
+
                             exchange.getRequest().mutate().header("username", response.getUsername());
                             exchange.getRequest().mutate().header("authorities", response.getAuthorities());
+
                             return exchange;
+
                         }).flatMap(chain::filter).onErrorResume(error -> {
+
                             log.info("Error Happened");
                             log.warn(error.getMessage() + "\n" + error.getCause());
-                            HttpStatusCode errorCode = null;
-                            String errorMsg = "";
+
+                            HttpStatusCode errorCode;
+                            String errorMsg;
+
+                            AuthenticationValidationFailedResponse authenticationValidationResponse = null;
+
                             if (error instanceof WebClientResponseException webClientException) {
+
                                 errorCode = webClientException.getStatusCode();
                                 errorMsg = webClientException.getStatusText();
 
+                                System.err.println(errorCode + "   " + errorMsg);
+
+                                authenticationValidationResponse = webClientException.getResponseBodyAs(AuthenticationValidationFailedResponse.class);
+                                if (authenticationValidationResponse != null) {
+                                    authenticationValidationResponse.setAuthenticated(false);
+                                    authenticationValidationResponse.setEndpoint(request.getURI().getPath());
+                                }
+
                             } else {
+
                                 errorCode = HttpStatus.BAD_GATEWAY;
                                 errorMsg = HttpStatus.BAD_GATEWAY.getReasonPhrase();
+
+                                authenticationValidationResponse = AuthenticationValidationFailedResponse.builder()
+                                        .isAuthenticated(false)
+                                        .status(HttpStatus.valueOf(errorCode.value()).name())
+                                        .code(errorCode.value())
+                                        .timestamp(new Date())
+                                        .token(bearerToken)
+                                        .endpoint(request.getURI().getPath())
+                                        .message(errorMsg)
+                                        .build();
                             }
-//                            AuthorizationFilter.AUTH_FAILED_CODE
-                            return onError(exchange, String.valueOf(errorCode.value()) ,errorMsg, "JWT Authentication Failed", (HttpStatus) errorCode);
+
+                            return onError(exchange, authenticationValidationResponse);
                         });
             }
 
@@ -79,21 +112,22 @@ public class AuthenticationPrefilter extends AbstractGatewayFilterFactory<Authen
         };
     }
 
-    private Mono<Void> onError(ServerWebExchange exchange, String errCode, String err, String errDetails, HttpStatus httpStatus) {
-        DataBufferFactory dataBufferFactory = exchange.getResponse().bufferFactory();
-//        ObjectMapper objMapper = new ObjectMapper();
-        ServerHttpResponse response = exchange.getResponse();
-        response.setStatusCode(httpStatus);
-        try {
-            response.getHeaders().add("Content-Type", "application/json");
+    private Mono<Void> onError(ServerWebExchange exchange, AuthenticationValidationFailedResponse validationResponse) {
 
-            byte[] byteData = objectMapper.writeValueAsBytes("error during authentication");
+        DataBufferFactory dataBufferFactory = exchange.getResponse().bufferFactory();
+        ServerHttpResponse response = exchange.getResponse();
+
+        response.setStatusCode(HttpStatusCode.valueOf(validationResponse.getCode()));
+
+        try {
+            response.getHeaders().add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+            byte[] byteData = objectMapper.writeValueAsBytes(validationResponse);
             return response.writeWith(Mono.just(byteData).map(dataBufferFactory::wrap));
 
         } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
+            log.error("Error was occurred during writing value to response body after authentication filter. " + e.getMessage());
+            return Mono.error(e);
         }
-
     }
 
     public static final List<String> openApiEndpoints = List.of(
@@ -101,7 +135,7 @@ public class AuthenticationPrefilter extends AbstractGatewayFilterFactory<Authen
             "/user/free"
     );
 
-    public Predicate<ServerHttpRequest> isSecured =
+    private static final Predicate<ServerHttpRequest> isSecured =
             request -> openApiEndpoints
                     .stream()
                     .noneMatch(uri -> request.getURI().getPath().contains(uri));
