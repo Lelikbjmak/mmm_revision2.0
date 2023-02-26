7d343bc9c68ee1c3812f60f4831fd92c8bce56bc
Lelikbjmak <denissolovey990@gmail.com>
2023-02-26 15:15:01 +0300

    AuthenticationValidationSuccessResponse to obtain info about token validation to reach any secured endpoint of other microservice

--- api-gateway/src/main/java/com/example/gateway/dto/AuthenticationValidationSuccessResponse.java
+++ api-gateway/src/main/java/com/example/gateway/dto/AuthenticationValidationSuccessResponse.java
@@ -5,16 +5,26 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.io.Serializable;
+import java.util.Date;
 
-@Data
 @Builder
-@NoArgsConstructor
+@Data
 @AllArgsConstructor
-public class AuthenticationValidationResponse implements Serializable {
+@NoArgsConstructor
+public class AuthenticationValidationSuccessResponse {
+
+    private Date timestamp;
+
     private String status;
+
+    private int code;
+
     private boolean isAuthenticated;
-    private String methodType;
+
     private String username;
+
     private String authorities;
+
+    private String methodType;
+
 }
