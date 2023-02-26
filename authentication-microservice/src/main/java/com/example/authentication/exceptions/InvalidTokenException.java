63f0b61884a2c6bd3ae78df5c4e10b92f5a4744a
Lelikbjmak <denissolovey990@gmail.com>
2023-02-26 15:17:12 +0300

    change lombok config for InvalidTokenException

--- authentication-microservice/src/main/java/com/example/authentication/exceptions/InvalidTokenException.java
+++ authentication-microservice/src/main/java/com/example/authentication/exceptions/InvalidTokenException.java
@@ -1,16 +1,20 @@
 package com.example.authentication.exceptions;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.Instant;
 
-@Data
+@Getter
 public class InvalidTokenException extends Exception {
 
     private final String token;
+
     private final Instant instant;
 
+    @Setter
     private String path = "/api/";
+
     public InvalidTokenException(String message, String token, Instant instant) {
         super(message);
         this.token = token;
