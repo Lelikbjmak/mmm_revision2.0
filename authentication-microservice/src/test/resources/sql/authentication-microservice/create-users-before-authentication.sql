f4fe1fb7ecc95a6bb7f0fa275e5c9cfc6a127a87
Lelikbjmak <denissolovey990@gmail.com>
2023-02-27 22:24:08 +0300

    Create users before AuthenticationControllerTest sql script

---
+++ authentication-microservice/src/test/resources/sql/authentication-microservice/create-users-before-authentication.sql
@@ -0,0 +1,21 @@
+-- create user before authentication test
+delete from users_roles;
+delete from users;
+
+insert into users values (1, null, 1, 1, 1, 1, 0, 'test1@gmail.com',
+'$argon2id$v=19$m=23552,t=2,p=1$vs6B0GzSxNN59ETxIeu8/Xs$sv7heef2KsSJfVFUpyq/kfndnN7s4p8e3TqQI79CogE',
+'testUser'), -- success user
+(2, '2030-01-01' , 1, 0, 1, 1, 3, 'test2@gmail.com',
+'$argon2id$v=19$m=23552,t=2,p=1$vs6B0GzSxNN59ETxIeu8/Xs$sv7heef2KsSJfVFUpyq/kfndnN7s4p8e3TqQI79CogE',
+'lockedUser'), -- locked user
+(3, null, 1, 1, 1, 0, 0, 'test3@gmail.com',
+'$argon2id$v=19$m=23552,t=2,p=1$vs6B0GzSxNN59ETxIeu8/Xs$sv7heef2KsSJfVFUpyq/kfndnN7s4p8e3TqQI79CogE',
+'disabledUser'), -- user disabled
+(4, null, 0, 1, 0, 1, 0, 'test4@gmail.com',
+'$argon2id$v=19$m=23552,t=2,p=1$vs6B0GzSxNN59ETxIeu8/Xs$sv7heef2KsSJfVFUpyq/kfndnN7s4p8e3TqQI79CogE',
+'expiredUser'); -- user expired
+
+insert into users_roles values
+(1, 1),
+(2, 1),
+(3, 1);
